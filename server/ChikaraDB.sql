7c1ae6d32f9dc7940f9d43ebefa620146c2c8c89
Marcus <sagisimarcus@gmail.com>
2023-10-03 14:18:04 -1000

    created exercises backend routes

--- server/ChikaraDB.sql
+++ server/ChikaraDB.sql
@@ -11,3 +11,38 @@ CREATE TABLE Users(
     Height NUMERIC(5, 2) NOT NULL,
     WEIGHT NUMERIC(5, 2) NOT NULL
 );
+
+CREATE TABLE Exercises (
+    ExerciseID SERIAL PRIMARY KEY,
+    UserID INT,
+    ExerciseName VARCHAR(255) NOT NULL,
+    Description TEXT,
+    Category VARCHAR(50),
+    UNIQUE(UserID, ExerciseName), 
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+);
+
+CREATE TABLE Workouts (
+    WorkoutID SERIAL PRIMARY KEY,
+    UserID INT,
+    PlanName VARCHAR(255) NOT NULL,
+    Description TEXT,
+    WorkoutDate DATE,
+    StartTime TIME,
+    EndTime TIME,
+    Notes TEXT,
+    CreationDate TIMESTAMPTZ DEFAULT current_timestamp,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+);
+
+CREATE TABLE WorkoutExercises (
+    WorkoutExerciseID SERIAL PRIMARY KEY,
+    WorkoutID INT,
+    ExerciseID INT,
+    Sets INT,
+    Reps INT,
+    Weight NUMERIC(5, 2), 
+    Duration INTERVAL,
+    FOREIGN KEY (WorkoutID) REFERENCES Workouts(WorkoutID),
+    FOREIGN KEY (ExerciseID) REFERENCES Exercises(ExerciseID)
+);
--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 import express from "express";
 import cors from "cors";
 import users from "./routes/users.js";
+import exercises from "./routes/exercises.js"
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/users", users);
-
+app.use("/exercises", exercises);
 export default app;
 
