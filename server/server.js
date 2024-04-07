aedd18b8bedadd89ce10f275d234ed0d9c0cb2ad
Marcus Sagisi <marcussagisi@Marcuss-MacBook-Pro.local>
2024-04-06 16:13:36 -1000

    Added workouts to app use

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors";
 import users from "./routes/users.js";
 import exercises from "./routes/exercises.js"
+import workouts from "./routes/workouts.js"
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/users", users);
 app.use("/exercises", exercises);
+app.use("/workouts", workouts)
 export default app;
 
